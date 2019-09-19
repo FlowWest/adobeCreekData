0e5c79643c2509e32adc111f2b1bf2a3a3f2732c
ERGZ <erodriguez@flowwest.com>
2019-09-19 13:37:05 -0700

    done one year 2005

--- data-raw/fish/fish.R
+++ data-raw/fish/fish.R
@@ -1,11 +1,18 @@
 library(tidyverse)
+library(lubridate)
 
 # Results -----------------------------------------------------------------
 
 # read in the data
+results_2005 <- read_csv("data-raw/fish/lakelive_results2005.csv")
+results_2005 %>% glimpse()
 
 # parse the data (make columns the corrent data type)
-
+results_2005 %>%
+  mutate(
+    datetime1 = ymd_h(paste0(date, " ", time)),
+    datetime2 = ymd_hm(paste0(date, " ", time))
+  ) %>% View()
 # plot
 
 # write data
