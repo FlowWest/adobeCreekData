41ddd43c8564dc4eccbd2fbb2ff4efc5c5143ac5
nsteinke <nsteinke@flowwest.com>
2019-09-19 15:44:02 -0700

    manipulated time strings to make times uniform, and added new datetime column

--- data-raw/fish/fish.R
+++ data-raw/fish/fish.R
@@ -4,33 +4,42 @@ library(lubridate)
 # Results -----------------------------------------------------------------
 
 # read in the data
-lakeslive_results_all <-
+lakelive_results_all <-
   list.files(path = "./data-raw/fish/",
     pattern="lakelive_results*",
     full.names = T) %>%
   map_df(~read_csv(.))
 
 #filter adobe creek data only
-lakelive_results_adobe <- filter(lakeslive_results_all, creek == "Adobe")
+lakelive_results_adobe <- filter(lakelive_results_all, creek == "Adobe")
 
 #comments on raw data:
 
-#-"time" entries are missing from certain year entirely
-#-"time" entries are in format H or HH instead of HH:MM
+#-"time" entries are missing from ~60% of entries
+#-"time" entries are in format H or HH instead of HH:MM in some observations
 #-location names are inconsistent: i.e. "Finley East" and "Finley East Road"
 #probably refer to same location. Likewise for "Soda Bay", "Soda Bay Rd", "SBR"
 #-location names are missing for some observations
-#"fish" values include  symbols ("?", "+", "-"), ranges of numbers ("75-100"), and text strings
-
-
+#"fish" values include  symbols ("?", "+", "-"), ranges of numbers ("75-100"),
+#and text strings
 
 
 # parse the data (make columns the corrent data type)
-results_2005 %>%
+# paste in :00 to times that only have hours
+
+
+lakelive_results_adobe <- lakelive_results_adobe %>%
   mutate(
-    datetime1 = ymd_h(paste0(date, " ", time)),
-    datetime2 = ymd_hm(paste0(date, " ", time))
-  ) %>% View()
+    time2 = if_else(str_length(time)== 4,
+      paste0(substr(time, 1, 2), ":00", substr(time, 3, 4)),
+        if_else((str_length(time)== 3),
+          paste0(substr(time, 1, 1),":00",substr(time, 2, 3)), time), time)
+  ) %>%
+
+#convert date and time to datetime column
+  mutate(
+    datetime = ymd_hm(paste0(date, " ", time2))
+  )
 # plot
 
 # write data
