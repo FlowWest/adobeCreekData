8f90a05ddae43c6ba9ba5c5d0d5e92181ed148d2
nsteinke <nsteinke@flowwest.com>
2019-09-24 15:49:30 -0700

    used case_when() to fix locations instead of a function. moved on to fixing fish column

--- data-raw/fish/fish.R
+++ data-raw/fish/fish.R
@@ -20,36 +20,22 @@ lakelive_results_adobe <- filter(lakelive_results_all, creek == "Adobe")
 # -location names are inconsistent: i.e. "Finley East" and "Finley East Road"
 # probably refer to same location. Likewise for "Soda Bay", "Soda Bay Rd", "SBR"
 # -location names are missing for some observations
-# "fish" values include  symbols ("?", "+", "-"), ranges of numbers ("75-100"),
+# -"fish" values include  symbols ("?", "+", "-"), ranges of numbers ("75-100"),
 # and text strings
 
 
 # parse the data (make columns the correct data type)--------------------
 
 
-# fixing time column -------------------------------
-# paste in :00 to times that only have hours
-
-# is there a function in R that eliminates the need for nested if_else?
+# fixing date column to date format-------------------------------
 
 lakelive_results_adobe <- lakelive_results_adobe %>%
   mutate(
-    time2 =
-      if_else(str_length(time)== 4,
-        paste0(substr(time, 1, 2), ":00", substr(time, 3, 4)),
-          if_else((str_length(time)== 3),
-            paste0(substr(time, 1, 1), ":00", substr(time, 2, 3)), time
-          ), time
-      )
-  ) %>%
-
-# convert date and time to datetime column
-
-  mutate(
-    datetime = ymd_hm(paste0(date, " ", time2))
+    date2= ymd(date)
   )
 
-# fixing location column IN PROGRESS ------------------------------------------
+
+# fixing location column ------------------------------------------
 
 # make locations all lowercase
 lakelive_results_adobe <- lakelive_results_adobe %>%
@@ -57,66 +43,41 @@ lakelive_results_adobe <- lakelive_results_adobe %>%
     location2 = tolower(location)
   )
 
-# initialize location3 column
-lakelive_results_adobe <- lakelive_results_adobe %>%
-  mutate(location3 = NA)
+# normalize string names
+lakelive_results_adobe2 <- lakelive_results_adobe %>%
+  mutate(location3 = case_when(
+    str_detect(location2, "argonaut") ~ "argonaut_rd",
+    str_detect(location2, "bell") ~ "bell_hill_rd",
+    str_detect(location2, "big valley") ~ "big_valley_rd",
+    str_detect(location2, "clear|mouth") ~ "clear_lake",
+    str_detect(location2, "finley") ~ "e_finley_rd",
+    str_detect(location2, "harris") ~ "harris_ave",
+    str_detect(location2, "highland|dam") ~ "highland_springs_dam",
+    str_detect(location2, "man") ~ "manning_br",
+    str_detect(location2, "merrit") ~ "merrit_rd",
+    str_detect(location2, "sbr") ~ "soda_bay_rd",
+    str_detect(location2, "scully") ~ "scully_meadow",
+    str_detect(location2, "soda") ~ "soda_bay_rd",
+    str_detect(location2, "stone") ~ "stone_rd",
+    str_detect(location2, "thomas") ~ "thomas_rd",
+    str_detect(location2, "29") ~ "hwy_29",
+    str_detect(location2, "14") ~ "14c-2"
+  ))
 
-# create function to help normalize strings
+# fixing "fish" column IN PROGRESS--------------------------------------
 
-# this function searches for search_criteria string in location2 column. if TRUE
-# it enters output_string in location3, if FALSE it leaves location3 as NA.
-location_fix <-
-  function(data_frame, search_criteria, output_string) {
-    if(is.na(location3)) {
-      mutate(
-        location3 = if(
-          grepl(search_criteria, location2)) {output_string}
-      )
-    }
-}
+lakelive_results_adobe3 <- lakelive_results_adobe2 %>%
+  mutate(fish2 = case_when(
+    str_detect(fish, "^\?") ~ NA,
+    str_detect(fish, "[+-]$") ~ substr(fish, 1, str_length(fish)-1)
+  ))
 
-# normalize locations beginning with argonaut
-
-
-lakelive_results_adobe <- lakelive_results_adobe %>%
-  location_fix("^argonaut", "argonaut_rd")
-
-lakelive_results_adobe <- lakelive_results_adobe %>%
-  if(is.na(location3)) {
-    mutate(
-      location3 = if(
-        grepl(search_criteria, location2)) {output_string}
-   )
-  }
-
-lakelive_results_adobe %>%
-  is.na(location2)
-
-#normalize locations beginning with finley
+lakelive_results_adobe2 %>%
   mutate(
-    location3 = if_else(grepl("^finley", location2), "finley_east_rd", location2)
-  )
+    fish2 = substr(fish, 1, str_length(fish)-1))
 
-lakelive_results_adobe %>%
-  location3 != location2
+str_detect(lakelive_results_adobe2$fish, "[+-]$")
 
-location_list = c()
-x = c("one" = 1, "two" = 2)
-
-x["one"]
-
-
-lakelive_results_adobe %>%
-  group_by(location) %>%
-  summarise( count = n()) %>%
-  view()
-
-lakelive_results_adobe <- lakelive_results_adobe %>%
-  mutate(
-    location2 =
-
-
-  )
 
 # plot----------------------------------------------------------------
 
